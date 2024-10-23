f2a6104013b943b2eda76de44cb1fa58146e92bd
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-10-23 16:23:48 +0200

    add-variableinit&utility

--- js/main.js
+++ js/main.js
@@ -7,3 +7,30 @@ const clearEl = document.querySelector("#clear-button");
 const displayEl = document.querySelector("#display");
 const userNameEl = document.querySelector("#user-name");
 const ticketPriceEl = document.querySelector("#ticket-price");
+
+// # Variable Init
+const pricePerKm = 0.21;
+const discountUnder18 = 0.8;
+const discountOver65 = 0.6;
+
+// # Utility Fn
+function ticketPriceCalc(tripLength, pricePerKm, age) {
+  let ticketPrice = tripLength * pricePerKm;
+  switch (age) {
+    case "Under 18":
+      ticketPrice *= discountUnder18;
+      break;
+    case "Over 65":
+      ticketPrice *= discountOver65;
+      break;
+    default:
+      break;
+  }
+  return `${ticketPrice} &euro;`;
+}
+
+function clearInput() {
+  fullnameEl.value = "";
+  tripLengthEl.value = "";
+  ageEl.value = "Scegli...";
+}
